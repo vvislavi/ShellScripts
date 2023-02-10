02c80f3619ea3caec6bec8bbe27761e277761116
Vytautas Vislavicius <sukcius@beast.hep.lu.se>
2023-02-10 15:29:25 +0100

    Preparing repo for Andy

--- mergeCorrOut.sh
+++
@@ -1,8 +0,0 @@
-function MergeCorrOutput() {
-    if [ -f MergedProcessed/${1}.root ] 
-    then
-	rm MergedProcessed/${1}.root
-    fi
-    hadd -f MergedProcessed/${1}.root Processed/*
-    rm Processed/*
-}
